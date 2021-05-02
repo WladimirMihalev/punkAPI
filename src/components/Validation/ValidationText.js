762846820139fb27704a3954dd12f3e26d36a956
Wladimir <32294647+WladimirMihalev@users.noreply.github.com>
2021-05-02 11:44:58 +0300

    Create ValidationText.js

---
+++ src/components/Validation/ValidationText.js
@@ -0,0 +1,12 @@
+const validationTextList = {
+    emptyName : "*Please enter your name.",
+    incorrectSymbUsed : "*Please enter alphabet characters only.", 
+    emptySurname : "*Please enter your surname.",
+    emptyEmail : "*Please enter your email-ID.", 
+    birthdayEmptyDate : "*Please enter your birthday date.",
+    emptyPassword : "*Please enter your password.",
+    weakPassword : "*Please enter secure and strong password."
+
+}
+ 
+export default validationTextList;
